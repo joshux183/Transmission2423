dd2ce3e0b1144ac18340ba218a042fa8f885391f
joshux183 <joshua.dawson364@gmail.com>
2025-04-11 23:07:36 +0100

    Create Age.py

---
+++ Age.py
@@ -0,0 +1,20 @@
+def ask_for_age_and_output():
+    age = input("Please enter your age: ")
+    
+#             ,   ,
+#        /////|
+#       ///// |
+#      /////  |
+#     |~~~| | |
+#     |===| |/|
+#     | B |/| |
+#     | I | | |
+#     | B | | |
+#     | L |  / 
+#     | E | /
+#     |===|/
+####  '---'
+    for _ in range(100):
+        print("11")
+
+ask_for_age_and_output()
